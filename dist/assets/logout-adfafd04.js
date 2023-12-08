f4d367ee8a4c78ddb8e26f484212295650429213
Binh Ong <binhot@pm.me>
2023-12-08 08:12:56 -0500

    Update

--- dist/assets/logout-adfafd04.js
+++
@@ -1,1 +0,0 @@
-import{a as o,F as e}from"./index-27b3262d.js";const n=()=>(document.cookie="",localStorage.removeItem("token"),window.location.href="/login",o(e,{children:"Logging out"}));export{n as default};
